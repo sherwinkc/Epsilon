37987c01698b4c96fb4ffea0d1a84d8ccb77561b
sherwinkc <sherwinclaridge@gmail.com>
2020-08-24 20:49:40 +0100

    0009

---
+++ Epsilon/Assets/Scripts/Parallax.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Parallax : MonoBehaviour
+{
+    public CinemachineVirtualCamera cam;
+    public float offset;
+    public float parallaxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //offset valu
+        offset = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3((cam.transform.position.x * parallaxSpeed) + (offset), transform.position.y, transform.position.z);
+        transform.position = new Vector3((cam.transform.position.x * parallaxSpeed) + (offset), transform.position.y, transform.position.z);
+
+        //DEBUG
+        /*Debug.Log("transform.position.x " + transform.position.x);
+        Debug.Log("offset: " + offset);
+        Debug.Log("cam.transform.position.x" + cam.transform.position.x);
+        Debug.Log("cam.transform.position.x * parallaxSpeed: " + cam.transform.position.x * parallaxSpeed);
+        Debug.Log("cam.transform.position.x * parallaxSpeed) + offset: " + (cam.transform.position.x * parallaxSpeed) + offset);*/
+    }
+}
