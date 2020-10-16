24052df2d6ab7751f4f30e37cfc993827b7d8261
sherwinkc <sherwinclaridge@gmail.com>
2020-10-16 14:42:22 +0100

    0039
    Docs

---
+++ Epsilon/Assets/Scripts/cameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class cameraController : MonoBehaviour
+{
+    public PlayerMovement playerMov;
+    public CinemachineVirtualCamera cam1;
+    public CinemachineVirtualCamera cam2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMov = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerMov.isGrounded && playerMov.canMove)
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+        }
+        else
+        {
+            cam1.enabled = true;
+            cam2.enabled = false;
+        }
+    }
+}
