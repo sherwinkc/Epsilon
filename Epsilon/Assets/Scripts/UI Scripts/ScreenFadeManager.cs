b45b169d300ca20c736f2c72087828ab57a95806
sherwinkc <sherwinclaridge@gmail.com>
2022-04-20 19:24:24 +0100

    0133
    Polished Screen Fade. In game and Main Menu. Buttons render under black image now

--- Epsilon/Assets/Scripts/UI Scripts/ScreenFadeManager.cs
+++ Epsilon/Assets/Scripts/UI Scripts/ScreenFadeManager.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenFadeManager : MonoBehaviour
 {
     Animator animator;
+    [SerializeField] Image img;
 
     //[SerializeField] bool turnOffCompletelyWhenNotRun = false;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-
         if (animator != null) animator.enabled = true;
+
+        img = GetComponent<Image>();
     }
 
     public void TurnOnAnimatorAndFadeOut()
     {
-        if (animator != null) 
+        TurnOnBlackImage();
+
+        if (animator != null)
         {
             animator.enabled = true;
             animator.Play("BlackScreenFadeOut");
@@ -28,10 +33,23 @@ public class ScreenFadeManager : MonoBehaviour
     {
         if (animator != null) animator.enabled = false;
         //if (turnOffCompletelyWhenNotRun) this.gameObject.SetActive(false);
+
+        Invoke("TurnOffBlackImage", 0.26f);
     }
 
     public void FadeIn()
     {
         animator.Play("BlackScreenFadeIn");
     }
+
+    public void TurnOffBlackImage()
+    {
+        if (img != null) img.enabled = false;
+    }
+
+    public void TurnOnBlackImage()
+    {
+        if (img != null) img.enabled = true;
+    }
 }
+
