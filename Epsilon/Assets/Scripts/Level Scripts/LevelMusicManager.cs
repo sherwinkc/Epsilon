9144c5be381200e3d0bb72483af1444792157716
sherwinkc <sherwinclaridge@gmail.com>
2022-05-22 19:26:41 +0100

    0168
    Somehow background blur is working!!!!!!!!!

--- Epsilon/Assets/Scripts/Level Scripts/LevelMusicManager.cs
+++ Epsilon/Assets/Scripts/Level Scripts/LevelMusicManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelMusicManager : MonoBehaviour
 {
     public AudioSource music1, music2, music3, music4, music5;
-    public bool playMusic1, playMusic2, playMusic3, playMusic4;
+    public bool randomiseMusic, playMusic1, playMusic2, playMusic3, playMusic4;
 
     private void Awake()
     {
@@ -53,6 +53,16 @@ public class LevelMusicManager : MonoBehaviour
             if (music2 != null) music2.gameObject.SetActive(false);
             if (music3 != null) music3.gameObject.SetActive(false);
             if (music4 != null) music4.gameObject.SetActive(true);
+        }
+        else if (randomiseMusic)
+        {
+            int var = Random.Range(1, 5);
+            Debug.Log(var);
+
+            if (var == 1) music1.gameObject.SetActive(true);
+            if (var == 2) music2.gameObject.SetActive(true);
+            if (var == 3) music3.gameObject.SetActive(true);
+            if (var == 4) music4.gameObject.SetActive(true);
         }
     }
 }
